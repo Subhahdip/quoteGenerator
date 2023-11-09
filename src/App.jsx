6a5a6a2d6e5dd3ab383c4c333bd4afba8875e6e1
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-09 21:14:38 +0530

    modified:   index.html 	new file:   logo.svg 	modified:   package-lock.json 	modified:   package.json 	new file:   postcss.config.js 	modified:   src/App.css 	modified:   src/App.jsx 	new file:   tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,18 @@ const App = () => {
 
   return (
     <div className="app-container">
-      <div className="background-container">
-        {advice && <p className="advice-text">{advice}</p>}
-
-        <button className="generate-button" onClick={fetchData}>
+      <div className=" w-full max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-950 dark:border-gray-700">
+        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
           Quote of the Day
+        </h5>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {advice}
+        </p>
+        <button
+          onClick={fetchData}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-teal-800 rounded-lg hover:bg-teal-900"
+        >
+          Read more
         </button>
       </div>
     </div>
