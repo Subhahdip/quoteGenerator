5d6ccd46710e4a60c727df3594e69f58872fe844
Subhadip Sarkar <edusubhadip@gmail.com>
2023-11-09 13:43:01 +0530

    modified:   src/App.css 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+const App = () => {
+  const [advice, setAdvice] = useState("");
 
-function App() {
-  return <>QuoteGenerator</>;
-}
+  const fetchData = async () => {
+    try {
+      const response = await fetch("https://api.adviceslip.com/advice");
+      if (response.ok) {
+        const data = await response.json();
+        setAdvice(data.slip.advice);
+      } else {
+        console.error("Failed to fetch advice");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <div className="background-container">
+        {advice && <p className="advice-text">{advice}</p>}
+
+        <button className="generate-button" onClick={fetchData}>
+          Quote of the Day
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
